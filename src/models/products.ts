a5f99ee09b94e5c7610aa1e6cde48307129f64cf
katyapank <ccoo00@yandex.ru>
2023-09-19 01:44:54 +0400

    products

--- src/models/products.ts
+++ src/models/products.ts
@@ -2,65 +2,65 @@ import Product from "@/types/product";
 
 const Products: Product[] = [
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: 'Когтеточка-столбик с игрушкой "Релакс"',
+        price: 481,
+        prevPrice: 500,
+        image: require("../../public/kogtetochka.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Керамическая мыльница со сливом",
+        price: 612,
+        prevPrice: 636,
+        image: require("../../public/milo.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Комбинезон нательный",
+        price: 547,
+        prevPrice: 564,
+        image: require("../../public/kombinezon.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Полотенца бумажные для диспенсера",
+        price: 358,
+        prevPrice: 908,
+        image: require("../../public/polotenca.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Мыльница",
+        price: 624,
+        prevPrice: 648,
+        image: require("../../public/milnica.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
+        title: "Комплект термобелья",
+        price: 2835,
         prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        image: require("../../public/termobelio.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Дозатор для жидкого мыла",
+        price: 687,
+        prevPrice: 701,
+        image: require("../../public/dozator.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Держатель для ванной комнаты",
+        price: 290,
+        prevPrice: 296,
+        image: require("../../public/derzhatel.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
+        title: "Комбинезон детский",
+        price: 685,
+        prevPrice: 721,
+        image: require("../../public/kombinezonb.jpg"),
     },
     {
-        title: 'Ремонт деревенских тапочек в Мурмаске',
-        price: 999,
-        prevPrice: 3580,
-        image: require('../../public/tapki.png')
-    }
-]
+        title: "Диспенсер для мыла механический",
+        price: 500,
+        prevPrice: 519,
+        image: require("../../public/dispenser.jpg"),
+    },
+];
 
-export default Products
+export default Products;
